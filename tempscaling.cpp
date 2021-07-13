508e0c1f319303dbd1f012dc62303e20a7bf1560
Paul Gavriloaea <paulgavriloaea@gmail.com>
2021-07-13 13:19:38 +0200

    Added tempscaling cpp and h files

---
+++ tempscaling.cpp
@@ -0,0 +1,25 @@
+//---This is the tempscaling.cpp file. It defines the tempscaling.h functions.
+
+
+//---Standard libraries
+
+
+//---User-defined libraries
+#include"tempscaling.h"
+
+//---Namespace tempscaling
+
+namespace tempscaling{
+	
+//---Variables
+
+
+
+//---Functions
+
+
+}
+
+
+//---End of tempscaling.cpp file.
+
---
+++ tempscaling.h
@@ -0,0 +1,26 @@
+//---This is the tempscaling.h file.
+#pragma once
+
+#ifndef TEMPSCALING_H
+#define TEMPSCALING_H
+
+//---Standard libraries
+
+
+//---User-defined libraries
+
+
+//---Namespace tempscaling
+
+namespace tempscaling{
+	
+//---Variables
+
+
+//---Functions
+
+
+}
+
+#endif
+//---End of tempscaling.h file.
